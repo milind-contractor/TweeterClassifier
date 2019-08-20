8052da5f8f05d53648deda0143414975e8e97682
milind-contractor <milind.newlife@gmail.com>
2019-08-20 09:21:54 +0800

    submit initial code

---
+++ Classsificastion.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import model_selection, naive_bayes, svm
+from sklearn.metrics import accuracy_score
+
+np.random.seed(500)
+
+
